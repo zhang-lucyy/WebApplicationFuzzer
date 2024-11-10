5658166ce0dff4f77b0de0612f23c14168d5509a
Lucy Zhang <lz3744@rit.edu>
2024-11-10 18:27:03 -0500

    testing guess

--- fuzzer.py
+++ fuzzer.py
@@ -1,17 +1,22 @@
 import argparse
 import mechanicalsoup
 
+# correctly guessed unlinked pages
+global pages_guessed
+
+# 
+global visited
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Fuzzer")
     parser.add_argument("command", choices=['discover'])
     parser.add_argument("url", type=str)
     parser.add_argument("--custom-auth", type=str)
+    parser.add_argument("--common-words", type=str)
+    parser.add_argument("--extensions", type=str)
     return parser.parse_args()
 
-def dvwa_auth(url):
-    # create browser object
-    browser = mechanicalsoup.StatefulBrowser()
-
+def dvwa_auth(url, browser):
     # Go to setup.php and reset the database
     browser.open(url + "/setup.php")
     browser.select_form('form[action="#"]')
@@ -33,16 +38,56 @@ def dvwa_auth(url):
     browser["security"] = "low"
     browser.submit_selected()
     
-    # print HTML content from home page - remove after fuzzer 0
+'''
+Uses the common word list to discover potentially unlinked pages by attempting
+every combination of word and extension.
+'''
+def guess(url, browser, args):
     browser.open(url)
-    print(browser.page)
+    words = args.common_words
+    exts = args.extensions
+
+    # try every word / extension combo
+    for word in words:
+        for ext in exts:
+            page = browser.open(url + "/" + word + "." + ext)
+            # link discovered
+            if (page.status_code == 200):
+                # add link to guessed pages set to keep track
+                pages_guessed.add(url + "/" + word + "." + ext)
+    for link in pages_guessed:
+        print(link + "\n")
+
+
+'''
+Discovers pages on the site by finding links and visiting them.
+
+def crawl(url, browser, args):
+    browser.open(url)
+    visited.add(url)
+    for link in browser.links:
+        href = link["href"]
+        absolute_url = 
+'''
+
+
+
+def discover(url):
+    guess(url)
+    
 
 def main():
+    pages_guessed = set()
+    vistited = set()
+
     args = parse_arguments()
+    # create browser object
+    browser = mechanicalsoup.StatefulBrowser()
     
     if args.command == "discover":
         if args.custom_auth == "dvwa":
-            browser = dvwa_auth(args.url)
+            browser = dvwa_auth(args.url, browser)
+            guess(args.url, browser, args)
 
 if __name__ == "__main__":
     main()
