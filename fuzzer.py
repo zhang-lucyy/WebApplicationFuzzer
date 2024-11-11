910a7cfb5e12efc8b26262cb0f2c834a92d6dc31
Lucy Zhang <lz3744@rit.edu>
2024-11-10 21:26:41 -0500

    testing guess and crawl

--- fuzzer.py
+++ fuzzer.py
@@ -7,6 +7,8 @@ global pages_guessed
 # 
 global visited
 
+
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Fuzzer")
     parser.add_argument("command", choices=['discover'])
@@ -26,12 +28,14 @@ def dvwa_auth(url, browser):
     browser.open(url)
     
     # Login with admin/password
+    print("LOGGING INTO DVWA")
     browser.select_form('form[action="login.php"]')
     browser["username"] = "admin"
     browser["password"] = "password"
     browser.submit_selected()
     
     # Set security level to Low
+    print("CHANGING SECURITY TO LOW")
     browser.open(url + "/security.php")
     browser.follow_link("security.php")
     browser.select_form('form[action="#"]')
@@ -43,37 +47,49 @@ Uses the common word list to discover potentially unlinked pages by attempting
 every combination of word and extension.
 '''
 def guess(url, browser, args):
-    browser.open(url)
-    words = args.common_words
-    exts = args.extensions
+    if args.common_words and args.extensions:
+        browser.open(url)
+        words = args.common_words
+        exts = args.extensions
 
-    # try every word / extension combo
-    for word in words:
-        for ext in exts:
-            page = browser.open(url + "/" + word + "." + ext)
-            # link discovered
-            if (page.status_code == 200):
-                # add link to guessed pages set to keep track
-                pages_guessed.add(url + "/" + word + "." + ext)
-    for link in pages_guessed:
-        print(link + "\n")
+        # try every word / extension combo
+        for word in words:
+            for ext in exts:
+                page = browser.open(url + "/" + word + "." + ext)
+                # link discovered
+                if (page.status_code == 200):
+                    # add link to guessed pages set to keep track
+                    pages_guessed.add(url + "/" + word + "." + ext)
 
 
 '''
 Discovers pages on the site by finding links and visiting them.
-
-def crawl(url, browser, args):
-    browser.open(url)
-    visited.add(url)
-    for link in browser.links:
-        href = link["href"]
-        absolute_url = 
 '''
+def crawl(url, browser, args):
+    url = browser.absolute_url(url)
+    page = browser.open(url)
+    if page is not None:
+        visited.add(url)
 
+    for link in browser.links():
+        href = link["href"]
+        # skip links that have already been visited or the same page or logout
+        if (href == "logout.php" or href in visited or href == url): continue
+        crawl(href)
 
 
 def discover(url):
     guess(url)
+    print("Pages Guessed")
+    print("*" * 15)
+    for link in pages_guessed:
+        print(link)
+
+    crawl(url)
+    print("Links Discovered")
+    print("*" * 15)
+    for link in visited:
+        print(link)
     
 
 def main():
@@ -84,10 +100,11 @@ def main():
     # create browser object
     browser = mechanicalsoup.StatefulBrowser()
     
+    # login to DVWA
+    if args.custom_auth == "dvwa":
+        dvwa_auth(args.url, browser)
     if args.command == "discover":
-        if args.custom_auth == "dvwa":
-            dvwa_auth(args.url, browser)
-            guess(args.url, browser, args)
+        guess(args.url, browser, args)
 
 if __name__ == "__main__":
     main()
